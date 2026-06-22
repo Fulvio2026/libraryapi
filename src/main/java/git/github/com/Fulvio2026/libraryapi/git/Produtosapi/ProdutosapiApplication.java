5c1cfbc732178beed8b93b6cff22d5fd98b56b2e
Fulvio Ferreira <fulvio.marlos@gmail.com>
2026-06-21 23:33:37 -0300

    first commit

---
+++ src/main/java/git/github/com/Fulvio2026/libraryapi/git/Produtosapi/ProdutosapiApplication.java
@@ -0,0 +1,13 @@
+package git.github.com.Fulvio2026.libraryapi.git.Produtosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/github/com/Fulvio2026/libraryapi/git/Produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package git.github.com.Fulvio2026.libraryapi.git.Produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
